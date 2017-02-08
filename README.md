5849121929f515a845cdebe237800f9a26e120f2
czhnja <czhnja@gmail.com>
2017-02-08 23:06:30 +0800

    20170208 10a

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Flasky ---- Zhnja 20170101
 ======
 20170104
 8d and add test case
+20170208
+10a
+before request don't work
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,26 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from ..models import User
 from .forms import LoginForm, RegistrationForm
 from .. import db
+
+
+# @auth.before_app_request
+# def before_request():
+#     if current_user.is_authenticated:
+#         current_user.ping()
+#         if not current_user.confirmed \
+#                 and request.endpoint[:5] != 'auth.' \
+#                 and request.endpoint != 'static':
+#             return redirect(url_for('auth.unconfirmed'))
+#
+#
+# @auth.route('/unconfirmed')
+# def unconfirmed():
+#     if current_user.is_anonymous or current_user.confirmed:
+#         return redirect(url_for('main.index'))
+#     return render_template('auth/unconfirmed.html')
 
 
 @auth.route('/login', methods=['GET', 'POST'])
